b01f38c17cf3c9405c76a02f3e7349ed7fca635a
Huzaifa Naseer Khan <huzaifanaseerkhan02@gmail.com>
2024-07-10 02:25:55 +0500

    Working on Django Templating Language

---
+++ meetups/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    meetups = [
+        {"title": 'A first meetup', "location": "Karachi", "slug": "a-first-meetup"},
+        {"title": 'A second meetup', "location": "Lahore", "slug": "a-second-meetup"}
+    ]
+
+    return render(request, "meetups/index.html", {
+        "show_meetups": True,
+        "meetups": meetups
+    })
